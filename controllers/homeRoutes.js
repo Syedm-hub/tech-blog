767307464909d85aed8e3f472aacd360f21e3015
Syed <82619273+Syedm-hub@users.noreply.github.com>
2021-09-07 15:22:25 -0700

    updated

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -84,3 +84,63 @@ router.get("/viewpost/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.get("/login", (req, res) => {
+  console.log("Is logged in?", req.session.loggedIn);
+  res.render("login", { loggedIn: req.session.loggedIn });
+});
+
+//setting up login page
+router.get("/dashboard", (req, res) => {
+  console.log(req.session.user.id, "this is the id");
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "body", "user_id"],
+    include: [
+      {
+        model: User,
+        as: "user",
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+        as: "comments",
+        attributes: ["id", "comment_text", "user_id"],
+        include: [
+          {
+            model: User,
+            as: "user",
+            attributes: ["username"],
+          },
+        ],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No Posts Available" });
+        return;
+      }
+      const posts = dbPostData.map((post) => post.get({ plain: true })); // serialize all the posts
+      console.log(posts);
+      res.render("dashboard", { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/post", (req, res) => {
+  res.render("create-post", { loggedIn: req.session.loggedIn });
+});
+
+router.get("/edit/:id", (req, res) => {
+  res.render("edit-post", {
+    loggedIn: req.session.loggedIn,
+    post_id: req.params.id,
+  });
+});
+module.exports = router;
