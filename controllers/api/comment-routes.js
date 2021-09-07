3428789b18ad1b0dbca314576a2e06030eacfe58
Syed <82619273+Syedm-hub@users.noreply.github.com>
2021-09-07 15:44:33 -0700

    updated

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -20,3 +20,30 @@ router.get("/", (req, res) => {
       res.status(400).json(err);
     });
 });
+
+router.get("/:id", (req, res) => {
+  Comment.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "comment_text", "user_id", "post_id"],
+    include: [
+      {
+        model: User,
+        as: "user",
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: "No Comment found with this id" });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
