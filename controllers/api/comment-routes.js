425bb877c65fcbf5ebbd74f2d7f242858ee87573
Syed <82619273+Syedm-hub@users.noreply.github.com>
2021-09-07 15:50:26 -0700

    updated

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -47,3 +47,18 @@ router.get("/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.post("/", (req, res) => {
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.session.user_id,
+    post_id: req.body.post_id,
+  })
+    .then((dbCommentData) => {
+      res.json(dbCommentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
